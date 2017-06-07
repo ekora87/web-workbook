37a07ab92013f8797073d2075a7ad444146a14c6
ekore87 <ekora87@gmail.com>
2017-06-06 19:49:04 -0500

    tic-tac-toe

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -1,5 +1,23 @@
 'use strict';
 
-$(document).on('ready', function() {
+$(document).ready(function() {
   // Put app logic in here
+  var playerTurn = "X";
+  var dataCell = $('[data-cell]');
+  var message = $('#announce-winner');
+
+  dataCell.click(function(){
+    if ($(this).text() === ""){
+      $(this).text(playerTurn);
+      message.empty();
+      playerTurn = (playerTurn === "X" ? "O" : "X");
+    }
+    else message.text("You can't click here");
+    });
+    
+  //Clear Board Function
+  $('#clear').click(function(){
+    dataCell.empty();
+    message.empty();
+  });
 });
